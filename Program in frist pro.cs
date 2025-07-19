1c43cd9055c4fccb24cafb4858a438053adbdd04
Kavitha018 <kavitha18022005@gmail.com>
2025-07-19 12:37:32 +0530

    Add files via upload

---
+++ Program in frist pro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Helllo
+{
+    static void Main(string[] args)
+    {
+
+        var i = 10;  //when we don't know the data type of a variable
+
+        var name = "Kavitha";
+
+        Console.WriteLine(i + " " + name);
+        //Console.WriteLine(sizeof(name));
+        
+    }
+  }
